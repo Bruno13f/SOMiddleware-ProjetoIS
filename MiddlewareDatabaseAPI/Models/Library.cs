dd69242818f72c97de3241c5324c713fd7bb035b
Bruno13f <bruno13f@outlook.pt>
2023-12-26 14:09:26 +0000

    Modelos s/ DB

---
+++ MiddlewareDatabaseAPI/Models/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiddlewareDatabaseAPI.Models
+{
+    public class Library
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime creation_dt { get; set; }
+    }
+}
---
+++ MiddlewareDatabaseAPI/Models/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiddlewareDatabaseAPI.Models
+{
+    public class Subscription
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime creation_dt { get; set; }
+        public string endpoint { get; set;}
+        public string event_mqqt { get; set; }
+        public int parent { get; set; }
+    }
+}
