e73a094b911b438df429f8a2581883d65e056e75
Bruno13f <bruno13f@outlook.pt>
2023-12-26 15:49:59 +0000

    Alteração Data

--- MiddlewareDatabaseAPI/Models/Data.cs
+++ MiddlewareDatabaseAPI/Models/Data.cs
@@ -8,6 +8,7 @@ namespace MiddlewareDatabaseAPI.Models
     public class Data
     {
         public int id { get; set; }
+        public string name {  get; set; }
         public string content { get; set; }
         public DateTime creation_dt { get; set; }
         public int parent { get; set;}
